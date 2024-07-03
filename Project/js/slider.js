35c5d1650fb2d3e59c9afeda220d09a445c13d06
ttipoya <124883294+ttipoya@users.noreply.github.com>
2024-07-03 17:05:42 +0400

    Add files via upload

--- Project/js/slider.js
+++ Project/js/slider.js
@@ -1,4 +1,8 @@
-const cardsSwiper = new Swiper(".js-swiper", {
+const width = document.querySelector('.first_page')
+const par = width.offsetWidth;
+console.log(par);
+if (par > 800){
+  const cardsSwiper = new Swiper(".js-swiper", {
     loop: true,
     slidesPerView: 2,
     spaceBetween: 30,
@@ -7,4 +11,18 @@ const cardsSwiper = new Swiper(".js-swiper", {
       prevEl: ".js-arrow-prev",
     },
   });
+}
+else if(par <= 800){
+  const cardsSwiper = new Swiper(".js-swiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".js-arrow-next",
+      prevEl: ".js-arrow-prev",
+    },
+  });
+}
+
+
   
